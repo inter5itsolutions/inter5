9bec0b6656df50542f6f8702c28c525ff24552b1
Blessing Akinola <akinolablessing25@gmail.com>
2026-04-08 17:47:43 +0100

    website UI improvement-error fix1

--- components/TestimonialSections.tsx
+++ components/TestimonialSections.tsx
@@ -31,7 +31,7 @@ const cardVariants: Variants = {
     opacity: 1,
     y: 0,
     transition: {
-      type: "spring",
+      type: "spring" as const,
       stiffness: 100,
       damping: 15,
     },
